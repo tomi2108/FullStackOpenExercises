bfd4ae45a56f4414123b4a824e15514390e7680e
tomi2108 <tominegrette@hotmail.com>
2022-03-03 00:30:07 -0300

    database added to notes

---
+++ notes/server/models/notes.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const url = process.env.MONGODB_URI;
+
+console.log("connecting to", url);
+
+mongoose
+  .connect(url)
+  .then(() => console.log("connected to MongoDB"))
+  .catch((err) => console.log("error connecting to MongoDB:", err));
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+});
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+module.exports = mongoose.model("Notes", noteSchema);
