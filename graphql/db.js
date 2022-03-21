14bdff7c73cbbc43065fc19eac9053e87e7a0124
Tomas <tominegrette@hotmail.com>
2022-03-21 12:16:17 -0300

    completed exercise 8.21

--- graphql/db.js
+++ graphql/db.js
@@ -1,5 +1,6 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
-const MONGODB_URI = "mongodb+srv://admin:admin@cluster0.lscfz.mongodb.net/graphQlDb?retryWrites=true&w=majority";
+const MONGODB_URI = process.env.MONGODB_URI; // eslint-disable-line no-undef
 
 module.exports = mongoose.connect(MONGODB_URI);
